19af9d0da9ed0f467e9436f60ebdcab78eb5ac46
Karlo Grlić <karlo@mail.com>
2024-09-17 23:31:44 +0200

    Introduce jinja2 as a templating engine

--- build.py
+++ build.py
@@ -2,14 +2,13 @@ import os
 import bs4
 import markdown
 import yaml
+import jinja2
 from typing import Any
 
 GENERATED_PAGES_DIR = './pages'
 MD_POSTS_DIR = './posts'
 INDEX_TEMPLATE_PAGE_PATH = './templates/index.html'
 POST_TEMPLATE_PAGE_PATH = './templates/post.html'
-POST_NAVIGATION_TEMPLATE_PATH = './templates/post-navigation.html'
-POST_ENTRY_TEMPLATE_PATH = './templates/post-entry.html'
 INDEX_PAGE_PATH = './pages/index.html'
 
 def load_posts() -> list[str]:
@@ -46,105 +45,26 @@ def generate_index_page(posts: list[dict[str, Any]]) -> None:
     with open(INDEX_TEMPLATE_PAGE_PATH, 'r') as template_file:
         index_template_html: str = template_file.read()
 
-    with open(POST_ENTRY_TEMPLATE_PATH, 'r') as template_file:
-        post_entry_html: str = template_file.read()
-
-    post_entries_html: str = ''
-
-    for post in posts:
-        post_entry_html_copy: str = post_entry_html
-        post_entry_html_copy = post_entry_html_copy.replace('{{ post_id }}', f'post-{post["id"]}')
-        post_entry_html_copy = post_entry_html_copy.replace('{{ post_title }}', post['title'])
-        post_entry_html_copy = post_entry_html_copy.replace('{{ post_description }}', post['description'])
-        post_entry_html_copy = post_entry_html_copy.replace('{{ post_date }}', post['date'].strftime("%B %d, %Y"))
-        post_entry_html_copy = post_entry_html_copy.replace('{{ post_url }}', post['url'])
-        post_entries_html += post_entry_html_copy + '\n'
-
-
-    # replace {{ post_entry_list }} with the post_entry_html
-    index_template_html = index_template_html.replace('{{ post_entry_list }}', post_entries_html)
-
-    soup: bs4.BeautifulSoup = bs4.BeautifulSoup(index_template_html, 'html.parser')
-
-    # format the soup and write it back to the file
-    formatter = bs4.formatter.HTMLFormatter(indent=4)
-    soup = soup.prettify(formatter=formatter)
+    index_template_html = jinja2.Template(index_template_html).render(posts=posts)
 
     with open(INDEX_PAGE_PATH, 'w') as index_page_file:
-        index_page_file.write(str(soup))
+        index_page_file.write(index_template_html)
 
 def generate_post_page(
-        previous_post: dict[str, str | int] | None,
         post: dict[str, str | int],
-        next_post: dict[str, str | int] | None
     ) -> None:
+
     with open(POST_TEMPLATE_PAGE_PATH, 'r') as template_file:
-        soup = bs4.BeautifulSoup(template_file, 'html.parser')
+        post_template_html: str = template_file.read()
 
-    # make a template html instead of this
-    post_div = soup.new_tag('article')
-    post_div['class'] = 'post'
-    post_div['id'] = f"post-{post['id']}"
-
-    post_header = soup.new_tag('header')
-
-    post_title:bs4.Tag = soup.new_tag('h1')
-    post_title['class'] = 'post-title'
-    post_title.string = post['title']
-    post_header.append(post_title)
-
-    post_date:bs4.Tag = soup.new_tag('time')
-    post_date['datetime'] = post['date'].strftime("%Y-%m-%d")
-    post_date.string = post['date'].strftime("%B %d, %Y")
-    post_date['class'] = 'post-date'
-    post_header.append(post_date)
-
-    post_div.append(post_header)
-    post_div.append(soup.new_tag('hr'))
-
-    with open(post['path'], 'r') as post_markdown_file:
-        post_markdown_content: str = post_markdown_file.read()
-        post_html_content: str = markdown.markdown(
-            post_markdown_content, 
-            extensions=['fenced_code', 'tables', 'meta']
-        )
-
-    post_div.append(bs4.BeautifulSoup(post_html_content, 'html.parser'))
-    soup.find('div', id="post-content").append(post_div)
-
-    # Inject the post navigation
-    with open(POST_NAVIGATION_TEMPLATE_PATH, 'r') as template_file:
-        post_navigation_html = template_file.read()
-
-    if previous_post:
-        post_navigation_html = post_navigation_html.replace('{{ previous_post_title }}', previous_post['title'])
-        post_navigation_html = post_navigation_html.replace('{{ previous_post_url }}', f'/{previous_post["url"]}')
-        post_navigation_html = post_navigation_html.replace('{{ previous_post_date }}', previous_post['date'].strftime("%B %d, %Y"))
-    else:
-        post_navigation_html = post_navigation_html.replace('{{ previous_post_title }}', '')
-        post_navigation_html = post_navigation_html.replace('{{ previous_post_url }}', '')
-        post_navigation_html = post_navigation_html.replace('{{ previous_post_date }}', '')
-
-    if next_post:
-        post_navigation_html = post_navigation_html.replace('{{ next_post_title }}', next_post['title'])
-        post_navigation_html = post_navigation_html.replace('{{ next_post_url }}', f'/{next_post["url"]}')
-        post_navigation_html = post_navigation_html.replace('{{ next_post_date }}', next_post['date'].strftime("%B %d, %Y"))
-    else:
-        post_navigation_html = post_navigation_html.replace('{{ next_post_title }}', '')
-        post_navigation_html = post_navigation_html.replace('{{ next_post_url }}', '')
-        post_navigation_html = post_navigation_html.replace('{{ next_post_date }}', '')
-
-    soup.find('div', id="post-navigation").append(bs4.BeautifulSoup(post_navigation_html, 'html.parser'))
-
-    # format the soup and write it back to the file
-    formatter = bs4.formatter.HTMLFormatter(indent=4)
-    soup = soup.prettify(formatter=formatter)
+    # Use jinja2 to render the post template
+    post_template_html = jinja2.Template(post_template_html).render(post=post)
 
     # Create directories if they don't exist
     os.makedirs(os.path.dirname(f'./pages/{post["url"]}/index.html'), exist_ok=True)
 
     with open(f'./pages/{post["url"]}/index.html', 'w') as post_page_file:
-        post_page_file.write(str(soup))
+        post_page_file.write(post_template_html)
 
 if __name__ == "__main__":
     # Make a copy of templates directory and rename it to pages
@@ -161,19 +81,20 @@ if __name__ == "__main__":
 
     # sort post_metas by date
     post_metas.sort(key=lambda x: x['date'], reverse=True)
-    post_id: int = 0
 
-    for metadata in post_metas:
-        metadata['id'] = post_id
+    for id, metadata in enumerate(post_metas):
+        metadata['id'] = id
+        metadata['previous'] = post_metas[id + 1] if id + 1 < len(post_metas) else None
+        metadata['next'] = post_metas[id - 1] if id - 1 >= 0 else None
+
+        with open(metadata['path'], 'r') as post_markdown_file:
+            metadata['content'] = markdown.markdown(
+                post_markdown_file.read(), 
+                extensions=['fenced_code', 'tables', 'meta']
+            )     
 
         # Generate the HTML page for the post
-        generate_post_page(
-            previous_post=post_metas[post_id + 1 ] if post_id + 1 < len(post_metas) else None,
-            post=metadata,
-            next_post=post_metas[post_id - 1] if post_id - 1 >= 0 else None
-        )
-
-        post_id += 1
+        generate_post_page(post=metadata)
 
     # Insert posts list into the index.html
     generate_index_page(post_metas)
