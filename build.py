e34563d2bc2c1e3f0993b2aeb2520c3155a35eb8
Karlo Grlić <karlo@mail.com>
2024-09-18 23:35:56 +0200

    Additional improvements to build script

--- build.py
+++ build.py
@@ -1,9 +1,11 @@
 import os
-import bs4
 import markdown
 import yaml
 import jinja2
+import datetime
+from dataclasses import dataclass, field
 from typing import Any
+from glob import glob
 
 GENERATED_PAGES_DIR = './pages'
 MD_POSTS_DIR = './posts'
@@ -11,37 +13,56 @@ INDEX_TEMPLATE_PAGE_PATH = './templates/index.html'
 POST_TEMPLATE_PAGE_PATH = './templates/post.html'
 INDEX_PAGE_PATH = './pages/index.html'
 
-def load_posts() -> list[str]:
-    posts: list[str] = []
-    for post in os.listdir(MD_POSTS_DIR):
-        if post.endswith('.md'):
-            posts.append(f"{MD_POSTS_DIR}/{post}")
+@dataclass
+class Post:
+    author: str = ''
+    content: str = ''
+    date: str = ''
+    description: str = ''
+    id: int = 0
+    keywords: list[str] = field(default_factory=list)
+    tags: list[str] = field(default_factory=list)
+    title: str = ''
+    url: str = ''
+
+def load_post(post_path: str) -> Post:
+    with open(post_path, 'r') as file:
+        content = file.read()
+    
+    metadata = extract_post_metadata(post_path)
+    
+    return Post(
+        content=markdown.markdown(content, extensions=['fenced_code', 'tables', 'meta']),
+        date=metadata.get('date', datetime.datetime.now().strftime('%Y-%m-%d')),
+        title=metadata.get('title', 'Untitled'),
+        description=metadata.get('description', 'No description'),
+        url=metadata.get('url', ''),
+        tags=metadata.get('tags', []),
+        keywords=metadata.get('keywords', []),
+        author=metadata.get('author', '')
+    )
+
+def load_posts(posts_dir: str = MD_POSTS_DIR) -> list[Post]:
+    # Load all posts from the posts directory and sort them by date
+    posts: list[Post] = sorted(
+        map(load_post, glob(os.path.join(posts_dir, '*', '*.md'))),
+        key=lambda post: post.date
+    )
+    
+    for i, post in enumerate(posts):
+        post.id = i
+        post.next = posts[i+1] if i < len(posts) - 1 else None
+        post.previous = posts[i-1] if i > 0 else None
 
     return posts
 
-def extract_metadata(post_path: str) -> dict[str, Any]:
+def extract_post_metadata(post_path: str) -> dict[str, Any]:
     with open(post_path, 'r') as file:
-        lines = file.readlines()
+        content = file.read().split('---', 2)    
+    
+    return yaml.safe_load(content[1].strip()) if len(content) >= 2 else {}
 
-    yaml_content = []
-    inside_yaml = False
-
-    for line in lines:
-        if line.strip() == '---':
-            if inside_yaml:
-                break
-            else:
-                inside_yaml = True
-        elif inside_yaml:
-            yaml_content.append(line)
-
-    yaml_content.append(f'path: {post_path}\n')
-
-    yaml_string = ''.join(yaml_content)
-    return yaml.safe_load(yaml_string)
-
-def generate_index_page(posts: list[dict[str, Any]]) -> None:
-
+def render_index_page(posts: list[Post]) -> None:
     with open(INDEX_TEMPLATE_PAGE_PATH, 'r') as template_file:
         index_template_html: str = template_file.read()
 
@@ -50,10 +71,7 @@ def generate_index_page(posts: list[dict[str, Any]]) -> None:
     with open(INDEX_PAGE_PATH, 'w') as index_page_file:
         index_page_file.write(index_template_html)
 
-def generate_post_page(
-        post: dict[str, str | int],
-    ) -> None:
-
+def render_post_page(post: Post) -> None:
     with open(POST_TEMPLATE_PAGE_PATH, 'r') as template_file:
         post_template_html: str = template_file.read()
 
@@ -61,9 +79,9 @@ def generate_post_page(
     post_template_html = jinja2.Template(post_template_html).render(post=post)
 
     # Create directories if they don't exist
-    os.makedirs(os.path.dirname(f'./pages/{post["url"]}/index.html'), exist_ok=True)
+    os.makedirs(os.path.dirname(f'./pages/{post.url}/index.html'), exist_ok=True)
 
-    with open(f'./pages/{post["url"]}/index.html', 'w') as post_page_file:
+    with open(f'./pages/{post.url}/index.html', 'w') as post_page_file:
         post_page_file.write(post_template_html)
 
 if __name__ == "__main__":
@@ -71,30 +89,11 @@ if __name__ == "__main__":
     os.system('rm -rf ./pages')
     os.system('cp -r ./templates ./pages')
 
-    post_metas: list[dict[str, Any]] = []
-    for post_path in load_posts():
-        post_metadata: dict[str, Any] = extract_metadata(
-            post_path=post_path
-        )
-        post_metas.append(post_metadata)
-        print(post_metadata)
-
-    # sort post_metas by date
-    post_metas.sort(key=lambda x: x['date'], reverse=True)
-
-    for id, metadata in enumerate(post_metas):
-        metadata['id'] = id
-        metadata['previous'] = post_metas[id + 1] if id + 1 < len(post_metas) else None
-        metadata['next'] = post_metas[id - 1] if id - 1 >= 0 else None
-
-        with open(metadata['path'], 'r') as post_markdown_file:
-            metadata['content'] = markdown.markdown(
-                post_markdown_file.read(), 
-                extensions=['fenced_code', 'tables', 'meta']
-            )     
-
-        # Generate the HTML page for the post
-        generate_post_page(post=metadata)
+    posts: list[Post] = load_posts()
 
     # Insert posts list into the index.html
-    generate_index_page(post_metas)
+    render_index_page(posts=posts)
+
+    for post in posts:
+        # Generate the HTML page for the post
+        render_post_page(post=post)
